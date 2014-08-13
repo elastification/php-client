88fb03c3007649d129f15ba8df002e2d65782442
DaWen <dan.wendlandt@gmail.com>
2014-08-13 17:27:34 +0200

    created new Unit tests for UpdateDocument and GetDocument

--- src/Request/V090x/DeleteDocumentRequest.php
+++ src/Request/V090x/DeleteDocumentRequest.php
@@ -9,7 +9,6 @@
 namespace Elastification\Client\Request\V090x;
 
 use Elastification\Client\Request\Shared\AbstractDeleteDocumentRequest;
-use Elastification\Client\Response\Response;
 use Elastification\Client\Response\ResponseInterface;
 use Elastification\Client\Response\V090x\DeleteDocumentResponse;
 use Elastification\Client\Serializer\SerializerInterface;
