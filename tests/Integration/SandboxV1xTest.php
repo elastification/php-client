2f86c2c6b3f3ee8dfcd15585163e1dadf33ce265
Mario Mueller <mueller@freshcells.de>
2014-08-13 10:35:03 +0200

    Added divergation for object or array deserialization, including test

---
+++ tests/Integration/SandboxV1xTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace Dawen\Component\Elastic\Tests\Integration;
+
+/**
+ * ${CARET}
+ * @package Dawen\Component\Elastic\Tests\Integration
+ * @author Mario Mueller <mueller@freshcells.de>
+ * @since 2014-08-13
+ * @version 1.0.0
+ */
+class SandboxV1xTest extends \PHPUnit_Framework_TestCase
+{
+
+    const INDEX = 'dawen-elastic';
+    const TYPE = 'sandbox';
+
+    private $url = 'http://localhost:9200/';
+
+    private $thriftClient;
+
+    public function setUp()
+    {
+
+    }
+
+    public function testThriftConnection()
+    {
+    }
+}
