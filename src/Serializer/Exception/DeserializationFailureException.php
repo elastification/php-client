5e4b2f5699160896412f63ad3a58223d1fa98984
Mario Mueller <mueller@freshcells.de>
2014-08-13 07:26:41 +0200

    Introduced Response data gateway.Json Serializer now validates a bit more and screams around if necessary.

---
+++ src/Serializer/Exception/DeserializationFailureException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Dawen\Component\Elastic\Serializer\Exception;
+
+/**
+ * ${CARET}
+ * @package Dawen\Component\Elastic\Serializer\Exception
+ * @author Mario Mueller <mueller@freshcells.de>
+ * @since 2014-08-13
+ * @version 1.0.0
+ */
+class DeserializationFailureException extends \RuntimeException
+{
+}
---
+++ src/Serializer/Gateway/GatewayInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dawen\Component\Elastic\Serializer\Gateway;
+/**
+ * Interface for accessing properties of the serialized data.
+ *
+ * Should only be used internally.
+ *
+ * @author Mario Mueller
+ */
+interface GatewayInterface extends \ArrayAccess
+{
+}
