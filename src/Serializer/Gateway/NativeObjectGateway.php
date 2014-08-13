c9583c2a6ac60fb07c695f848ff0474f271e728e
Mario Mueller <mueller@freshcells.de>
2014-08-13 21:44:44 +0200

    Added test for non existent property on the object gateway

--- src/Serializer/Gateway/NativeObjectGateway.php
+++ src/Serializer/Gateway/NativeObjectGateway.php
@@ -50,12 +50,15 @@ class NativeObjectGateway implements GatewayInterface
      */
     public function offsetGet($offset)
     {
-        $value = $this->jsonData->$offset;
-        if (is_scalar($value)) {
-            return $value;
-        } else {
-            return new NativeObjectGateway($this->jsonData->$offset);
+        if (property_exists($this->jsonData, $offset)) {
+            $value = $this->jsonData->$offset;
+            if (is_scalar($value)) {
+                return $value;
+            } else {
+                return new NativeObjectGateway($this->jsonData->$offset);
+            }
         }
+        return null;
     }
 
     /**
