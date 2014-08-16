2c8b4c93a1dc039a951e8e03387b397949939e0c
DaWen <dan.wendlandt@gmail.com>
2014-08-16 10:58:26 +0200

    refactored abstract requests. Created AbstractBaseRequest

--- src/Request/Shared/AbstractCreateDocumentRequest.php
+++ src/Request/Shared/AbstractCreateDocumentRequest.php
@@ -13,65 +13,9 @@ use Elastification\Client\Request\RequestMethods;
 use Elastification\Client\Response\ResponseInterface;
 use Elastification\Client\Serializer\SerializerInterface;
 
-abstract class AbstractCreateDocumentRequest implements RequestInterface
+abstract class AbstractCreateDocumentRequest extends AbstractBaseRequest
 {
-    /**
-     * @var \Elastification\Client\Serializer\SerializerInterface
-     */
-    private $serializer;
-
-    /**
-     * @var array
-     */
-    private $serializerParams = array();
-
-    /**
-     * @var null|string
-     */
-    private $index = null;
-
-    /**
-     * @var null|string
-     */
-    private $type = null;
-
     private $body = null;
-
-    /**
-     * @param string $index
-     * @param string $type
-     * @param \Elastification\Client\Serializer\SerializerInterface $serializer
-     * @param array $serializerParams
-     */
-    public function __construct($index, $type, SerializerInterface $serializer, array $serializerParams = array())
-    {
-        $this->serializer = $serializer;
-        $this->serializerParams = $serializerParams;
-
-        if(!empty($index)) {
-            $this->index = $index;
-        }
-
-        if(!empty($type)) {
-            $this->type = $type;
-        }
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getIndex()
-    {
-        return $this->index;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
 
     /**
      * @inheritdoc
@@ -87,22 +31,6 @@ abstract class AbstractCreateDocumentRequest implements RequestInterface
     public function getAction()
     {
         return null;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getSerializer()
-    {
-        return $this->serializer;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getSerializerParams()
-    {
-        return $this->serializerParams;
     }
 
     /**
