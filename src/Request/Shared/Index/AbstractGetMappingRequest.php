8e01efa1f7921770e2338bf18aef6181d2c58807
DaWen <dan.wendlandt@gmail.com>
2014-08-20 16:44:21 +0200

    modev getMAppingRequest into index

---
+++ src/Request/Shared/Index/AbstractGetMappingRequest.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+namespace Elastification\Client\Request\Shared\Index;
+
+use Elastification\Client\Request\RequestMethods;
+use Elastification\Client\Request\Shared\AbstractBaseRequest;
+
+abstract class AbstractGetMappingRequest extends AbstractBaseRequest
+{
+
+    const REQUEST_ACTION = '_mapping';
+
+    /**
+     * @inheritdoc
+     */
+    public function getMethod()
+    {
+        return RequestMethods::GET;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAction()
+    {
+        return self::REQUEST_ACTION;
+    }
+
+    /**
+     * get the body
+     *
+     * @return mixed
+     * @author Daniel Wendlandt
+     */
+    public function getBody()
+    {
+        return null;
+    }
+
+    /**
+     * before setting data it should be serialized
+     *
+     * @param mixed $body
+     * @throws \Elastification\Client\Exception\RequestException
+     * @author Daniel Wendlandt
+     */
+    public function setBody($body)
+    {
+        // do nothing here
+    }
+
+}
