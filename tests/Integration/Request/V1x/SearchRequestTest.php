cfba3b0d4b1bb70abcb8caaecb8e8db6ce2372c6
DaWen <dan.wendlandt@gmail.com>
2015-11-20 13:47:29 +0100

    Added integration test for v1x SearchRequest

---
+++ tests/Integration/Request/V1x/SearchRequestTest.php
@@ -0,0 +1,136 @@
+<?php
+namespace Elastification\Client\Tests\Integration\Request\V1x;
+
+
+use Elastification\Client\Request\V1x\SearchRequest;
+use Elastification\Client\Response\V1x\SearchResponse;
+
+class SearchRequestTest extends AbstractElastic
+{
+
+    const TYPE = 'request-search';
+
+    public function testMatchAllSearch()
+    {
+        $this->createIndex();
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $this->refreshIndex();
+
+        $searchRequest = new SearchRequest(ES_INDEX, self::TYPE, $this->getSerializer());
+
+        $query = [
+            "query" => [
+                "match_all" => []
+            ]
+        ];
+
+        $searchRequest->setBody($query);
+        /** @var SearchResponse $response */
+        $response = $this->getClient()->send($searchRequest);
+
+
+        $this->assertGreaterThan(0, $response->took());
+        $this->assertFalse($response->timedOut());
+        $shards = $response->getShards();
+        $this->assertTrue(isset($shards['total']));
+        $this->assertTrue(isset($shards['successful']));
+        $this->assertTrue(isset($shards['failed']));
+        $this->assertGreaterThan(2, $response->totalHits());
+        $this->assertGreaterThan(0, $response->maxScoreHits());
+
+        $hits = $response->getHits();
+        $this->assertArrayHasKey('total', $hits);
+        $this->assertArrayHasKey('max_score', $hits);
+        $this->assertArrayHasKey('hits', $hits);
+
+        $hitsHits = $response->getHitsHits();
+        foreach ($hitsHits as $hit) {
+            $this->assertSame(ES_INDEX, $hit['_index']);
+            $this->assertSame(self::TYPE, $hit['_type']);
+        }
+    }
+
+    public function testMatchAllSearchWithParamSize()
+    {
+        $this->createIndex();
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $this->refreshIndex();
+
+        $searchRequest = new SearchRequest(ES_INDEX, self::TYPE, $this->getSerializer());
+
+        $query = [
+            "query" => [
+                "match_all" => []
+            ]
+        ];
+
+        $searchRequest->setBody($query);
+        $searchRequest->setParameter('size', 1);
+
+        /** @var SearchResponse $response */
+        $response = $this->getClient()->send($searchRequest);
+
+        $this->assertGreaterThan(0, $response->took());
+        $this->assertFalse($response->timedOut());
+        $shards = $response->getShards();
+        $this->assertTrue(isset($shards['total']));
+        $this->assertTrue(isset($shards['successful']));
+        $this->assertTrue(isset($shards['failed']));
+        $this->assertGreaterThan(2, $response->totalHits());
+        $this->assertGreaterThan(0, $response->maxScoreHits());
+
+        $hitsHits = $response->getHitsHits();
+        $this->assertCount(1, $hitsHits);
+    }
+
+    public function testMatchAllSearchWithParamScroll()
+    {
+        $this->createIndex();
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $this->refreshIndex();
+
+        $timeStart = microtime(true);
+        $searchRequest = new SearchRequest(ES_INDEX, self::TYPE, $this->getSerializer());
+
+        $query = [
+            "query" => [
+                "match_all" => []
+            ]
+        ];
+
+        $searchRequest->setBody($query);
+        $searchRequest->setParameter('scroll', '1m');
+        $searchRequest->setParameter('search_type', 'scan');
+
+        /** @var SearchResponse $response */
+        $response = $this->getClient()->send($searchRequest);
+
+        echo 'search(with scroll params): ' . (microtime(true) - $timeStart) . 's' . PHP_EOL;
+
+        $responseData = $response->getData()->getGatewayValue();
+
+        $this->assertTrue(isset($responseData['_scroll_id']));
+        $this->assertGreaterThan(5, strlen($responseData['_scroll_id']));
+    }
+}
