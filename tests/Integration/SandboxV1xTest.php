2e38bdbaf200281b0d2e98fb2034786ba9c2a9de
DaWen <dan.wendlandt@gmail.com>
2015-11-20 14:59:55 +0100

    Added integration test for v1x SearchScrollRequest

--- tests/Integration/SandboxV1xTest.php
+++ tests/Integration/SandboxV1xTest.php
@@ -239,56 +239,7 @@ class SandboxV1xTest extends \PHPUnit_Framework_TestCase
 //
 //
 
-//
-//    public function testSearchScroll()
-//    {
-//        $this->createIndex();
-//        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
-//        $this->createDocument($data);
-//        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
-//        $this->createDocument($data);
-//        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
-//        $this->createDocument($data);
-//        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
-//        $this->createDocument($data);
-//        $this->refreshIndex();
-//
-//
-//        $searchRequest = new SearchRequest(self::INDEX, self::TYPE, $this->serializer);
-//
-//        $query = [
-//            "query" => [
-//                "match_all" => []
-//            ]
-//        ];
-//
-//        $searchRequest->setBody($query);
-//        $searchRequest->setParameter('scroll', '1m');
-//        $searchRequest->setParameter('search_type', 'scan');
-//
-//        /** @var SearchResponse $response */
-//        $response = $this->client->send($searchRequest);
-//        $responseData = $response->getData()->getGatewayValue();
-//        $this->assertTrue(isset($responseData['_scroll_id']));
-//
-//
-//        $timeStart = microtime(true);
-//        $scrollRequest = new SearchScrollRequest(self::INDEX, self::TYPE, $this->serializer);
-//        $scrollRequest->setScroll('1m');
-//        $scrollRequest->setScrollId($responseData['_scroll_id']);
-//
-//        $responseScroll = $this->client->send($scrollRequest);
-//
-//        echo 'searchScroll: ' . (microtime(true) - $timeStart) . 's' . PHP_EOL;
-//
-//        $scrollData = $responseScroll->getData()->getGatewayValue();
-//        $scrollHits = $responseScroll->getHitsHits();
-//
-//        $this->assertTrue(isset($scrollData['_scroll_id']));
-//        $this->assertGreaterThan(5, strlen($scrollData['_scroll_id']));
-//
-//        $this->assertCount(4, $scrollHits);
-//    }
+
 //
 //    public function testGetMappingWithType()
 //    {
