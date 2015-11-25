bb5b85ef1ab2f7c3cf2e9611bfc9c5fb64aa15a2
DaWen <dan.wendlandt@gmail.com>
2015-11-25 08:50:04 +0100

    Added new integration tests for V090x Index/GetMappingRequest

---
+++ tests/Integration/Request/V090x/Index/GetMappingRequestTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Elastification\Client\Tests\Integration\Request\V090x\Index;
+
+
+use Elastification\Client\Request\V090x\Index\GetMappingRequest;
+use Elastification\Client\Response\ResponseInterface;
+use Elastification\Client\Tests\Integration\Request\V090x\AbstractElastic;
+
+class GetMappingRequestTest extends AbstractElastic
+{
+    const TYPE = 'request-index-get-mapping';
+
+    public function testGetMappingWithType()
+    {
+        $this->createIndex();
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $this->refreshIndex();
+
+        $getMappingRequest = new GetMappingRequest(ES_INDEX, self::TYPE, $this->getSerializer());
+
+        /** @var ResponseInterface $response */
+        $response = $this->getClient()->send($getMappingRequest);
+
+        $this->assertContains(self::TYPE, $response->getRawData());
+        $this->assertContains('"properties":', $response->getRawData());
+        $this->assertContains('"name":', $response->getRawData());
+        $this->assertContains('"value":', $response->getRawData());
+    }
+
+    public function testGetMappingWithoutType()
+    {
+        $this->createIndex();
+        $data = array('name' => 'test' . rand(100, 10000), 'value' => 'myTestVal' . rand(100, 10000));
+        $this->createDocument(self::TYPE, $data);
+        $this->refreshIndex();
+
+        $getMappingRequest = new GetMappingRequest(ES_INDEX, null, $this->getSerializer());
+
+        /** @var ResponseInterface $response */
+        $response = $this->getClient()->send($getMappingRequest);
+
+        $this->assertContains(ES_INDEX, $response->getRawData());
+        $this->assertContains(self::TYPE, $response->getRawData());
+        $this->assertContains('"properties":', $response->getRawData());
+        $this->assertContains('"name":', $response->getRawData());
+        $this->assertContains('"value":', $response->getRawData());
+    }
+}
