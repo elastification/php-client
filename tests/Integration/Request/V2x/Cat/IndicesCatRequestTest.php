3488d00cb99d3d80e1cdb50b558bfecdc4097c19
DaWen <dan.wendlandt@gmail.com>
2016-01-05 18:34:34 +0100

    Added V2x IndicesCatRequest and tests

---
+++ tests/Integration/Request/V2x/Cat/IndicesCatRequestTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Elastification\Client\Tests\Integration\Request\V2x\Cat;
+
+
+use Elastification\Client\Request\V2x\Cat\IndicesCatRequest;
+use Elastification\Client\Response\Response;
+use Elastification\Client\Tests\Integration\Repository\V2x\AbstractElastic;
+
+class IndicesCatRequestTest extends AbstractElastic
+{
+    const TYPE = 'request-cat-indices';
+
+    public function testHealthCat()
+    {
+        $this->createDocument(self::TYPE);
+        $this->createDocument(self::TYPE);
+        $this->refreshIndex();
+
+        $countCatRequest = new IndicesCatRequest(null, null, $this->getSerializer());
+        /** @var Response $response */
+        $response = $this->getClient()->send($countCatRequest);
+
+        $data = $response->getData();
+        $this->assertCount(1, $data);
+
+        $index = $data->getGatewayValue()[0];
+
+        $this->assertGreaterThanOrEqual(9, $index);
+
+        $this->assertArrayHasKey('health', $index);
+        $this->assertArrayHasKey('status', $index);
+        $this->assertArrayHasKey('index', $index);
+        $this->assertArrayHasKey('pri', $index);
+        $this->assertArrayHasKey('rep', $index);
+        $this->assertArrayHasKey('docs.count', $index);
+        $this->assertArrayHasKey('docs.deleted', $index);
+        $this->assertArrayHasKey('store.size', $index);
+        $this->assertArrayHasKey('pri.store.size', $index);
+
+        $this->assertSame(ES_INDEX, $index['index']);
+        $this->assertEquals(2, $index['docs.count']);
+
+    }
+
+}
