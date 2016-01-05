1428a216263e10ceb2641c66e920ff5615e9c59f
DaWen <dan.wendlandt@gmail.com>
2016-01-05 18:45:11 +0100

    Added V2x MasterCatRequest and tests

---
+++ tests/Integration/Request/V2x/Cat/MasterCatRequestTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Elastification\Client\Tests\Integration\Request\V2x\Cat;
+
+
+use Elastification\Client\Request\V2x\Cat\MasterCatRequest;
+use Elastification\Client\Response\Response;
+use Elastification\Client\Tests\Integration\Repository\V2x\AbstractElastic;
+
+class MasterCatRequestTest extends AbstractElastic
+{
+    public function testHealthCat()
+    {
+        $countCatRequest = new MasterCatRequest(null, null, $this->getSerializer());
+        /** @var Response $response */
+        $response = $this->getClient()->send($countCatRequest);
+
+        $data = $response->getData();
+        $this->assertCount(1, $data);
+
+        $index = $data->getGatewayValue()[0];
+
+        $this->assertGreaterThanOrEqual(4, $index);
+
+        $this->assertArrayHasKey('id', $index);
+        $this->assertArrayHasKey('host', $index);
+        $this->assertArrayHasKey('ip', $index);
+        $this->assertArrayHasKey('node', $index);
+
+    }
+
+}
